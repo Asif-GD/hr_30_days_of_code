837f6f63685c0f046b7a4659905ba00fb025d804
Asif-GD <asif.coach@gmail.com>
2024-11-28 13:15:19 +0530

    solve day_1

---
+++ 001_data_types/001_problem.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i_2: [int] = int(input("enter your integer: "))
+d_2: [float] = float(input("enter your floating point number: "))
+s_2: [str] = input("enter your string: ").lower()
+# Print the sum of both integer variables on a new line.
+print(i + i_2)
+# Print the sum of the double variables on a new line.
+print(d + d_2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s_2)
